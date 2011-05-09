109886c18efcd6580b1adffb253a7aa45aacfadb
vklein <v.klein@iconsultants.eu>
2011-05-09 16:42:33 +0200

    init config

--- init.php
+++ init.php
@@ -33,11 +33,11 @@ Zend_Db_Table::setDefaultAdapter($db);
 $global=new ArrayObject();
 $global->db=$db;
 
-require_once 'lib/app-arena/Helper.php';
+require_once '/lib/app-arena/Helper.php';
 $helper = new Helper();
 
 // Initialize App-Arena Connection
-require_once 'lib/app-arena/client/soap_client.php';
+require_once '/lib/app-arena/client/soap_client.php';
 $apparena = new Client($aa_api_key);
 $instance_id = 0;
 $page_id=get_page_id();

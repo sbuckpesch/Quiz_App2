0da901b90e54fc7aa690a1d9448d89cc5a271386
vklein <v.klein@iconsultants.eu>
2011-05-12 16:58:16 +0200

    cookie

--- welcome.php
+++ welcome.php
@@ -14,8 +14,8 @@ $data = json_decode(base64_decode(strtr($payload, '-_', '+/')), true);
 
 // Get Cookie which is set if you have used this App before
 $values = $_COOKIE;
-if(isset($_COOKIE['img_participant']))
-print_r ($values['img_participant']);
+if(isset($_COOKIE['instance_id']['$instid']))
+print_r ("Ja");
 
 // Get content from App-Arena
 $columns = $config['number_column'];

7a0ae259acb73ad6293eba495469a1a80adbfca7
vklein <v.klein@iconsultants.eu>
2011-05-13 14:18:47 +0200

    debugg

--- welcome.php
+++ welcome.php
@@ -52,7 +52,7 @@ function fbAuthAndSubmit(appID, scope) {
 			function(response) {				
 			  	if (response.perms != null && isSetProperSubset(scope.split(","), response.perms.split(","))) {
 //					alert("Berechtigung erteilt.");
-					alert(jQuery("#formX").html()) ; 
+//					alert(jQuery("#formX").html()) ; 
 					document.getElementById("formX").submit();
 				}
 			}

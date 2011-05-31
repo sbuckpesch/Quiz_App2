a50be8371b067cf48efae6d477d2410b0b4d6df0
fred <fred@iconsultants.eu>
2011-05-31 21:48:15 +0800

    update

--- template/result_type2.php
+++ template/result_type2.php
@@ -1,34 +1,7 @@
 <?php
 
-//print_r($this->params);
+print_r($this->params);
 
 ?>
-<h4></h4>
+<h4>Finished Question</h4>
 
-<div class="eft">
-  <div class="q_result">
-    <div class="result_b">
-      <div class="question">
-
-            <div class="c_name"> 1. faw </div>
-            <div class="content">
-              <div class="clear"></div>
-              <div class="choice">
-                <div class="your">Your Choice:</div>
-                <div class="y_name">a</div>
-              </div>
-              
-              <div class="res_img"><img src="http://d1bye8fl443jlj.cloudfront.net/prod/images/x_image.png"></div>
-              
-              <div class="correct">
-                <div class="t_name">Correct Answer:</div>
-                <div class="ans">feaw</div>
-              </div>
-              
-              <div class="clear"></div>
-            </div>
-
-      </div>
-    </div>
-  </div>
-</div>

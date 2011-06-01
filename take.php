2cfbbb021e96dc01cd45f81a674e13022046e2c3
fred <fred@iconsultants.eu>
2011-06-01 19:47:30 +0800

    update soap call

--- take.php
+++ take.php
@@ -77,16 +77,32 @@
     <?php endforeach; ?>
     </form>
     <a href="#" onclick="take_quiz();return false;"> Teilnehmen</a>
-<?php else: ?>
+<?php endif; ?>
+<?php if($is_admin == true): ?>
   <!-- create link -->
   <hr/>
+   
+  <?php if($quiz == false): ?>
+  <!-- create link -->
   <div>
-    <a onclick="load_page('create_type.php',{id:1});return false;">
     <a target="_top" href="<?php echo $global->appurl; ?>create_type.php">
+      Create
     </a>
   </div>
-  
+  <?php else: ?>
+  <!-- edit link -->
+  <?php 
+    $quiz=(object) $quiz; 
+    $quiz_id=$quiz->id;
+  ?>
+  <div>
+  <a target="_top" href="<?php echo $global->appurl; ?>create_type.php?quiz_id=<?php echo $quiz_id ?>">
+      Edit
+    </a>
+  </div>
+  <?php endif; ?>
 <?php endif; ?>
+  
 <!-- Footer -->
 <div style="background-image: url(images/iventus/bg3.jpg); width: 492px; height: 100px;">
 </div>

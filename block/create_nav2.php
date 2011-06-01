8e4c2543f335233ac2103e37dafca1f821cbd753
fred <fred@iconsultants.eu>
2011-06-01 17:31:32 +0800

    update

--- block/create_nav2.php
+++
@@ -1,9 +0,0 @@
-    <a id="quiz_part_nav_1" class="step1" onclick="show_part1()">Part 1 <br>
-      <span>Enter quiz info</span>
-    </a>
-    <a id="quiz_part_nav_2" class="step2" onclick="show_part2()">Part 2 <br>
-    <span>Create quiz questions</span>
-    </a>
-    <a id="quiz_part_nav_3" class="step3" onclick="show_part3()">Part 3 <br>
-      <span>Preview and publish quiz</span>
-    </a>
--- create_part3.php
+++
@@ -1,5 +0,0 @@
-<?php load("block/publish_edit.php"); ?>
-
-<?php render("block/preview.php",array('quiz_id'=>16)); ?>
-
-<?php load("block/publish_edit.php"); ?>

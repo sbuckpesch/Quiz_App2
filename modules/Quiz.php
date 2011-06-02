89b6144f0f3018180a180217b12bdb9573cc785d
fred <fred@iconsultants.eu>
2011-06-02 14:17:50 +0800

    update

--- modules/Quiz.php
+++ modules/Quiz.php
@@ -37,9 +37,7 @@ class Quiz
 
     $select->from(Config::Question_Table,"*");
     $select->where('quiz_id=?',$quiz_id);
-    //$select->order('id desc');
-    //
-    echo $select;
+    $select->order('id asc');
 
     $rows=$db->fetchAll($select);
 

cff15611ae81c6a014230b6edaffc1d26f202d91
fred <fred@iconsultants.eu>
2011-06-03 12:55:50 +0800

    update

--- index.php
+++ index.php
@@ -5,10 +5,13 @@ $init->initDb();
 $init->initFluttery();
 $page_id=$init->initPageId();
 
+
+var_dump($global);
 echo '<span style="color:red">FF</span>';
 
 include('header.php'); 
 
-include('take.php');
+
+//include('take.php');
 include('footer.php'); 
 ?>

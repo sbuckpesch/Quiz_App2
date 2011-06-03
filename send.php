0dcc739ed786c78f93b65962f9ad9461c6d95532
fred <fred@iconsultants.eu>
2011-06-03 16:18:40 +0800

    update

--- send.php
+++ send.php
@@ -3,4 +3,51 @@ require_once('init.php');
 
 $page_id=$_GET['page_id'];
 
-echo $page_id;
+var_dump($page_id);
+/*
+$init->initFluttery(0,$page_id);
+
+$to_email=$global->config['email'];
+echo $to_email;
+
+if($to_email != false)
+{
+
+  $email=$_POST['email'];
+  if($email != false)
+  {
+    $firstname=$_POST['firstname'];
+    $name=$_POST['name'];
+    $city=$_POST['city'];
+    $postcode=$_POST['postcode'];
+    $address=$_POST['address'];
+
+
+    $message=<<<HTML
+     From Quiz   \n
+    ---------------- \n
+    Vorname: $firstname \n
+    Name: $name  \n
+    Adresse: $address  \n
+    Ort: $city \n
+    PLZ:$postcode  \n
+    Email: $email \n
+HTML;
+
+
+    $subject = "Hotel"; 
+    $headers='';
+    $headers .= "From: " . $name . " <" . $email . ">\r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-type: text/html; charset=utf-8\r\n";
+    $headers .= "Content-Transfer-encoding: 8bit\r\n";
+    $headers .= "X-Mailer: php" . phpversion(); 
+
+    $ret=mail($to_email,$subject,$message,$headers);
+
+    var_dump($ret);
+  }
+
+}
+ */
+?>

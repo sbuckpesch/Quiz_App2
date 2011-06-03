37fae556a51bf8285dbb620953dca1c98386d9aa
fred <fred@iconsultants.eu>
2011-06-03 19:51:46 +0800

    update

--- modules/Init.php
+++ modules/Init.php
@@ -49,7 +49,7 @@ class Init
 
     $app_key='fred';
 
-    $fluttery = new Fluttery($app_key,$gsession,false);
+    $fluttery = new Fluttery($app_key,$gsession,true);
     if($instid >0 )
       $fluttery->setInstanceId($instid);
     else

c6c22b089935b098cd35bd09eb4b2b0b3872693c
fred <iamlosing02@gmail.com>
2011-07-26 17:10:21 +0800

    update

--- init.php
+++ init.php
@@ -4,7 +4,7 @@
  */
 // Load config values
 error_reporting(E_ALL|E_STRICT);
-//ini_set('display_errors', 1);
+ini_set('display_errors', 1);
 date_default_timezone_set('Europe/Berlin');
 
 //auto load

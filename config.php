40dd137c624e002eedd128a2c2563a599f55684a
Sebastian Buckpesch <s.buckpesch@iconsultants.eu>
2011-08-31 19:15:38 +0200

    Remove DB-Data.

--- config.php
+++ config.php
@@ -3,16 +3,11 @@ $ic_app_id=48;
 
 $database_host='localhost';
 
-$aa_app_key='3a73f7ca99a84d7fe2dd42b156293e77';
+$aa_app_key='';
 
 
-$database_name='ic_aa_quiz';
-$database_user='ic_aa_quiz';
-$database_pass='WaMSsYtMwbhX3Z7XfEvN';
+$database_name='';
+$database_user='';
+$database_pass='';
 
-/*
-$database_user='root';
-$database_pass='123';
-$database_name='quiz';
-*/
 ?>
